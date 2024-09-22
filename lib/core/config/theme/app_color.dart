0ad52201e09824a383c4dc09e8322b3f6da181cd
ali-I-salhab <alysalhab6@gmail.com>
2024-09-22 11:52:11 +0300

    finish theming

---
+++ lib/core/config/theme/app_color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xff42C83C);
+  static const Color lightbackground = Color(0xfff2f2f2);
+
+  static const Color darkbackground = Color(0xff0d0c0c);
+
+  static const Color grey = Color(0xffbebebe);
+  static const Color darkgrey = Color(0xff343434);
+}
---
+++ lib/core/config/theme/app_theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:spotify/core/config/theme/app_color.dart';
+
+class AppTheme {
+  static final lighttheme = ThemeData(
+      brightness: Brightness.light,
+      primaryColor: AppColors.primary,
+      scaffoldBackgroundColor: AppColors.lightbackground,
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30)),
+              backgroundColor: AppColors.primary,
+              textStyle:
+                  const TextStyle(fontSize: 20, fontWeight: FontWeight.bold))));
+
+  static final darktheme = ThemeData(
+      brightness: Brightness.dark,
+      primaryColor: AppColors.primary,
+      scaffoldBackgroundColor: AppColors.darkbackground,
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30)),
+              backgroundColor: AppColors.primary,
+              textStyle:
+                  const TextStyle(fontSize: 20, fontWeight: FontWeight.bold))));
+}
