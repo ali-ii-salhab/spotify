2c0e6af146819ccda5a7711c299e2641284b7a3a
ali-I-salhab <alysalhab6@gmail.com>
2024-09-22 12:37:02 +0300

    end assets

--- lib/core/config/theme/app_theme.dart
+++ lib/core/config/theme/app_theme.dart
@@ -3,6 +3,7 @@ import 'package:spotify/core/config/theme/app_color.dart';
 
 class AppTheme {
   static final lighttheme = ThemeData(
+      fontFamily: "Satoshi",
       brightness: Brightness.light,
       primaryColor: AppColors.primary,
       scaffoldBackgroundColor: AppColors.lightbackground,
@@ -15,6 +16,7 @@ class AppTheme {
                   const TextStyle(fontSize: 20, fontWeight: FontWeight.bold))));
 
   static final darktheme = ThemeData(
+      fontFamily: "Satoshi",
       brightness: Brightness.dark,
       primaryColor: AppColors.primary,
       scaffoldBackgroundColor: AppColors.darkbackground,
