c221560edfbb06dc829254d8ef521ebacdf744ce
ali-I-salhab <alysalhab6@gmail.com>
2024-09-24 09:31:21 +0300

    end splash

---
+++ lib/presentation/spalsh/pages/spalsh.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Spalsh extends StatelessWidget {
+  const Spalsh({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Image.asset()));
+  }
+}
